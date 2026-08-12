65ad1ce7a4e56b34ce25fd868fd494f6c0ced878
Nabil <116415990+nabilrn@users.noreply.github.com>
2026-08-12 09:32:08 +0700

    wire global application header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import '../app.css';
+	import AppHeader from '$components/AppHeader.svelte';
 	import Navbar from '$components/Navbar.svelte';
 	import Toast  from '$components/Toast.svelte';
 	import { page } from '$app/stores';
@@ -39,8 +40,9 @@
 {#if checked || isPublic}
 	{#if !isPublic && user}
 		<div class="min-h-screen transition-[padding] duration-200 {$sidebarCollapsed ? 'lg:pl-16' : 'lg:pl-64'}">
-			<Navbar {user} />
-			<main class="min-h-screen">
+			<Navbar />
+			<AppHeader {user} />
+			<main class="min-h-[calc(100vh-3.5rem)] lg:min-h-[calc(100vh-4rem)]">
 				<slot />
 			</main>
 		</div>
