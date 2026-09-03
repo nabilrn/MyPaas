2c303225733557d14dce2587479721973865d81e
Nabil <116415990+nabilrn@users.noreply.github.com>
2026-09-04 02:47:01 +0700

    ux: let source settings use parent inset

--- frontend/src/routes/projects/[id]/settings/source/+page.svelte
+++ frontend/src/routes/projects/[id]/settings/source/+page.svelte
@@ -2,6 +2,4 @@
 	import ProjectSettingsSection from '$components/ProjectSettingsSection.svelte';
 </script>
 
-<div class="px-4">
-	<ProjectSettingsSection section="source" />
-</div>
+<ProjectSettingsSection section="source" />
